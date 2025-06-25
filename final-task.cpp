c7e15fd55bb1dd00b14499b67bcfbf0f5fc7cb41
Redfield-13 <fieldr62@gmail.com>
2025-06-25 11:40:24 +0200

    spellings correction + extra spaces removed

--- final-task.cpp
+++ final-task.cpp
@@ -1,25 +1,21 @@
 #include <iostream>;
 #include<iomanip>
 using namespace std;
-// global varible
+// global variable
 float total_be,discount, room_price,final_cost;
 int id,book_rooms,occupant;
 string room;
 
 //untile here
 
-
-
-//the declartion of the function;
+//the declaration of the function;
 int CalcFee(){
-
-
 //user input(id)
 	cout<<"enter the room id you want to Book: ";
 		cin>>id;
 		cout<<endl;
-		//user input(quntity)
-		cout<<"enter the quntity of rooms to book: ";
+		//user input(quantity)
+		cout<<"enter the quantity of rooms to book: ";
 		cin>>book_rooms;
 		cout<<endl;
 		//user input (occupant)
@@ -28,8 +24,7 @@ int CalcFee(){
 		cout<<endl<<endl<<endl;
 		//end user input;
 		
-		
-		//the condtions of id;
+		//the conditions of id;
 			if(id==0){
 		room="single room";
 		room_price=50;
@@ -38,34 +33,24 @@ int CalcFee(){
 		room="Double room";
 		room_price=90;
 	}else if(id==2){
-		room="Dluex room";
+		room="Deluxe room";
 		room_price=150;
 		
 	}else if(id==3){
 		room="suite";
 		room_price=250;
 	}
-	//end condtion;
-	
-	
+	//end condition;
 		
 //total price befor discount
 	total_be=book_rooms*room_price;
 	
-	
-	
-	
-	
 }
-//end of the function
 
-
-
-
-//declartion of the function
+//declaration of the function
 int getDisc(){
 
-	//the condtion 
+	//the condition 
 	if (total_be>180){
 		//the discount
 		discount=total_be*0.05;
@@ -73,31 +58,22 @@ int getDisc(){
 	final_cost=	total_be-discount;
 	
 	}
-	
-	
-	
-
 }
 //end of function;
 
 
-
-
-
-
-
 int main(){
 
-//basicaly all of this for just to make the user see our Avalible Rooms;	
+//Basically all of this for just to make the user see our Available Rooms;	
 string names[]={"single room","Double Room","Deluxe Room","Suite"};
 string prices[]={"50.00","90.00","150.00","     250.00"};
 
 string occupants[]={"1","2","4","6"};
 
-//welcime massage;
+//welcome massage;
 cout<<setw(67)<<"WELCOME IN OUR HOTEL "<<endl<<endl<<endl;
 
-	cout<<"Availble Rooms:"<<endl;
+	cout<<"Available Rooms:"<<endl;
 	cout<<endl<<"Room id"<<setw(13)<<"Room type"<<setw(28)<<"price per Room(RM)"<<setw(25)<<"Max occupants"<<endl<<endl;
 	cout<<"-----------------------------------------------------------------------------------"<<endl;
 	
@@ -113,7 +89,7 @@ cout<<endl<<endl<<endl;
 		getDisc();
 	//untile here;
 		
-		//condtion of number of occupants
+		//condition of number of occupants
 		
 	if (occupant>=1){
 		cout<<"==================== Booking Summery====================="<<endl<<endl;
@@ -129,7 +105,7 @@ cout<<endl<<endl<<endl;
 	//untile here;
 	
 	
-	//condtion of price more than 180;
+	//condition of price more than 180;
 	if(total_be>180){
 			cout<<"Total price (befor Discount): RM "<<fixed<<setprecision(2)<<total_be<<endl<<endl;
 			cout<<"Discount Amount (5%): RM "<<discount<<endl<<endl;
